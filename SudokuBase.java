b2dcdf67c0e70afc6c59dc1850d70cff9aea36e4
Gyurjyan <goharik.gyurjyan@etu.umontpellier.fr>
2023-12-05 11:29:21 +0100

    verif test

--- SudokuBase.java
+++ SudokuBase.java
@@ -516,20 +516,10 @@ public class SudokuBase {
      *               et affiche qui a gagné
      */
     public static void main(String[] args){
-        /*int gagnant = partie();
+        int gagnant = partie();
         if (gagnant == 0) System.out.println("C'est un match nul !");
         else if (gagnant == 1) System.out.println("L'humain a gagné !");
-        else System.out.println("L'ordinateur a gagné !");*/
-        int[][] S={{6,2,9,7,8,1,3,4,5},
-		   {4,7,3,9,6,5,8,1,2},
-		   {8,1,5,2,4,3,6,9,7},
-		   {9,5,8,3,1,2,4,7,6},
-		   {7,3,2,4,5,6,1,8,9},
-		   {1,6,4,8,7,9,2,5,3},
-		   {3,8,1,5,2,7,9,6,4},
-		   {5,9,6,1,3,4,7,2,8},
-		   {2,4,7,6,9,8,5,3,1}};
-	    SudokuBase.afficheGrille(3,S);
+        else System.out.println("L'ordinateur a gagné !");
      // fin main
     }
 } // fin SudokuBase
